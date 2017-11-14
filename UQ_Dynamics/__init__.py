99c350320e0ab2394180ad6edfd16bb65bf41c65
arpanmuk <arpanmuk@buffalo.edu>
2017-11-14 14:04:02 -0500

    Deleted

--- UQ_Dynamics/__init__.py
+++ UQ_Dynamics/__init__.py
@@ -1,7 +1,8 @@
 from clustering_algorithms import ClusteringMethod
 from linearization import Linearization, JacobianLinearization, StatisticalLinearization
 from sampling import Sample, UT, MonteCarlo, SamplingMethod, GaussQuadrature, Moment, Hadamard, SamplingMethodGenerator
+from kl_expansion import KLExpansion
 
 __all__ = ['ClusteringMethod','Linearization', 'JacobianLinearization', 'StatisticalLinearization',
            'Sample', 'UT', 'MonteCarlo', 'SamplingMethod', 'GaussQuadrature', 'Moment', 'Hadamard',
-           'SamplingMethodGenerator']
+           'SamplingMethodGenerator','KLExpansion']
---
+++ UQ_Dynamics/kl_expansion.py
@@ -0,0 +1,43 @@
+import numpy as np
+from scipy.sparse.linalg.eigen.arpack import eigsh as largest_eigsh
+
+class KLExpansion:
+
+    def __init__(self, cov_function=None, precomputed=True):
+
+        """
+        Initialize KL Expansion with covariance function
+        if precomputed is set to True, then must provide cov_function
+        :param cov_function:
+        :param precomputed:
+        """
+        if precomputed and cov_function is None:
+            raise ValueError('if precomputed is set to True, then must provide cov_function')
+
+        if cov_function is not None:
+            self.cov_function = cov_function
+
+    def compute_k_matrix(self, domain, tol=1e-8):
+
+        x = domain[0]
+        y = domain[1]
+
+        k = self.cov_function(x, y)
+
+        k[k < tol] = 0
+
+        return k
+
+    def compute_eigen_functions(self, domain, num_eigens):
+
+        X = self.compute_k_matrix(domain)
+        return largest_eigsh(X, num_eigens, which='LM')
+
+    def cluster_region(self, map_data, cluster_matrix):
+
+        region = []
+        for cluster in cluster_matrix:
+            clust = cluster.astype(bool)
+            sub_map = map_data[clust]
+            domain = np.meshgrid(sub_map, sub_map)
+            region.append(self.compute_k_matrix(domain))
